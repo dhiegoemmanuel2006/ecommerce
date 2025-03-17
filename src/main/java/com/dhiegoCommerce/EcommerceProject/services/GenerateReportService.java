b8198429e4116b872753a40b6f5a8c3838c6526f
dhiegoemmanuel2006 <dhiegoemmanuelsouza@gmail.com>
2025-03-16 23:55:05 -0300

    ADD: Criação do GenerateReportService para aplicar as funcionalidades da geração de PDF

--- src/main/java/com/dhiegoCommerce/EcommerceProject/services/GenerateReportService.java
+++ src/main/java/com/dhiegoCommerce/EcommerceProject/services/GenerateReportService.java
@@ -1,26 +1,85 @@
 package com.dhiegoCommerce.EcommerceProject.services;
 
+import com.dhiegoCommerce.EcommerceProject.entities.Product;
+import com.dhiegoCommerce.EcommerceProject.repositories.ProductRepository;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.UnitValue;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverAction;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.sql.*;
+import java.util.List;
 
 @Service
 public class GenerateReportService {
-    private String url = "jdbc:mysql://localhost:3306/jdbc:mysql://localhost:3306/faculdadescrum";
-    private String user = "root";
-    private String password = "root";
+    private final ProductService pService;
 
     private String pdfPath = "C:" + File.separator + "resultados" + File.separator + "relatorio.pdf";
 
-    Logger log =
+    Logger log = LoggerFactory.getLogger(GenerateReportService.class);
 
-    public void generateReport() throws SQLException {
-        Connection db = DriverManager.getConnection(url, user, password);
+    public GenerateReportService(ProductService pService) {
+        this.pService = pService;
+    }
+
+    public void generateReport() throws FileNotFoundException {
+        try{
+            log.info("Iniciando a geração do pDF");
+            List<Product> list = pService.findAllProductsTable();
+            if(list == null || list.isEmpty()){
+                throw new Exception("Lista vazia");
+            }
+
+            log.info("Criando documento!");
+            PdfWriter writer =  new PdfWriter(new FileOutputStream(pdfPath));
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+
+            Paragraph titulo = new Paragraph("Relatório de Produtos")
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setFontSize(18)
+                    .setBold();
+            document.add(titulo);
+
+            document.add(new Paragraph("\n"));
+
+
+            log.info("Gerando a tabela!");
+            Table table = new Table(UnitValue.createPercentArray(6)).useAllAvailableWidth();
+
+            table.addHeaderCell("id");
+            table.addHeaderCell("name");
+            table.addHeaderCell("description");
+            table.addHeaderCell("price");
+            table.addHeaderCell("quantity");
+            table.addHeaderCell("valor total");
+
+            for(Product product : list){
+                table.addCell(String.valueOf(product.getId()));
+                table.addCell(product.getName());
+                table.addCell(product.getDescription());
+                table.addCell(String.format("R$ %.2f", product.getPrice()));
+                table.addCell(String.valueOf(product.getQuantity()));
+                table.addCell(String.format("R$ %.2f", product.getQuantity() * product.getPrice()));
+            }
+
+            document.add(table);
+            document.close();
+            log.info("Relatorio gerado com sucesso!");
+        } catch (Exception e) {
+            log.error("Falha ao gerar relatório: " + e.getMessage());
+        }
+
     }
 
 }
