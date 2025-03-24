0db5e0019131560fa5ff0129022411888c537496
dhiegoemmanuel2006 <dhiegoemmanuelsouza@gmail.com>
2025-03-23 23:40:51 -0300

    update: Melhoria na geração dos PDFs

--- src/main/java/com/dhiegoCommerce/EcommerceProject/controllers/GeneratePdfController.java
+++ src/main/java/com/dhiegoCommerce/EcommerceProject/controllers/GeneratePdfController.java
@@ -1,6 +1,7 @@
 package com.dhiegoCommerce.EcommerceProject.controllers;
 
 import com.dhiegoCommerce.EcommerceProject.services.GenerateReportService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +18,13 @@ public class GeneratePdfController {
     }
 
 
-    @GetMapping("/gerarpdf")
-    public void generatePdf(HttpServletResponse response) throws IOException {
+    @GetMapping("/gerarpdf/product")
+    public void generatePdfForProduct(HttpServletResponse response) throws IOException {
         grService.generateReport(response);
+    }
+
+    @GetMapping
+    public void generatePdfForMovimentation(HttpServletResponse response) throws IOException {
+        grService.generateReportForMovimentation(response);
     }
 }
--- src/main/java/com/dhiegoCommerce/EcommerceProject/services/GenerateReportService.java
+++ src/main/java/com/dhiegoCommerce/EcommerceProject/services/GenerateReportService.java
@@ -1,5 +1,6 @@
 package com.dhiegoCommerce.EcommerceProject.services;
 
+import com.dhiegoCommerce.EcommerceProject.entities.Movimentation;
 import com.dhiegoCommerce.EcommerceProject.entities.Product;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -21,12 +22,14 @@ import java.util.List;
 @Service
 public class GenerateReportService {
     private final ProductService pService;
+    private final MovimentationService mService;
 
 
     Logger log = LoggerFactory.getLogger(GenerateReportService.class);
 
-    public GenerateReportService(ProductService pService) {
+    public GenerateReportService(ProductService pService, MovimentationService mService) {
         this.pService = pService;
+        this.mService = mService;
     }
 
     public void generateReport(HttpServletResponse response) throws IOException {
@@ -38,7 +41,7 @@ public class GenerateReportService {
             PdfDocument pdf = new PdfDocument(writer);
             Document document = new Document(pdf);)
         {
-            log.info("Iniciando a geração do pDF");
+            log.info("Iniciando a geração do PDF das movimentações");
             List<Product> list = pService.findAllProductsTable();
             if(list == null || list.isEmpty()){
                 throw new Exception("Lista vazia");
@@ -78,12 +81,66 @@ public class GenerateReportService {
 
             document.add(table);
             response.getOutputStream().flush();
-            log.info("Relatorio gerado com sucesso!");
+            log.info("Relatorio de produtos gerado com sucesso!");
         } catch (Exception e) {
-            log.error("Falha ao gerar relatório: " + e.getMessage());
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao gerar o relatório");
+            log.error("Falha ao gerar relatório de produtos: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao gerar o relatório de produtos");
         }
 
     }
 
+    public void generateReportForMovimentation(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"relatorio.pdf\"");
+
+
+        try(PdfWriter writer =  new PdfWriter(response.getOutputStream());
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);)
+        {
+            log.info("Iniciando a geração do PDF das movimentações");
+            List<Movimentation> list = mService.getAll();
+            if(list == null || list.isEmpty()){
+                throw new Exception("Lista vazia");
+            }
+
+            log.info("Criando documento para as movimentações!");
+
+
+
+            Paragraph titulo = new Paragraph("Relatório das movimentações")
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setFontSize(18)
+                    .setBold();
+            document.add(titulo);
+
+            document.add(new Paragraph("\n"));
+
+
+            log.info("Gerando a tabela!");
+            Table table = new Table(UnitValue.createPercentArray(4)).useAllAvailableWidth();
+
+            table.addHeaderCell("id");
+            table.addHeaderCell("name_produto");
+            table.addHeaderCell("quantidade");
+            table.addHeaderCell("horário");
+
+
+            for(Movimentation m : list){
+                table.addCell(String.valueOf(m.getId()));
+                table.addCell(m.getNomeProduto());
+                table.addCell(String.valueOf(m.getQuantityOfProduct()));
+                table.addCell(String.valueOf(m.getHorarioOperacao()));
+
+            }
+
+            document.add(table);
+            response.getOutputStream().flush();
+            log.info("Relatorio de movimentações gerado com sucesso!");
+        } catch (Exception e) {
+            log.error("Falha ao gerar relatório de movimentações: " + e.getMessage());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao gerar o relatório de movimentações");
+        }
+
+    }
 }
